112950eff394a9f98830ff382b772bdd0accead8
degenerasian <tycedric.a@gmail.com>
2024-10-21 11:31:37 +0800

    Commit files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProjectCardComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
