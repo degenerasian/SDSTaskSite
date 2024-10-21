f2e9c11ad567cf23ef36883f95d1d8ff3ecddae1
degenerasian <tycedric.a@gmail.com>
2024-10-21 15:18:20 +0800

    Remove Angular Framework
    - removed angular component for simpler integration with PHP

---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>SDSTaskSite</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body class="bg-body-tertiary">
+
+<!--nav-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="" alt="SDS Logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item px-3">
+                    <a class="nav-link active text-white" aria-current="page" href="#">Projects</a>
+                </li>
+                <div class="vr text-white"></div>
+                <li class="nav-item px-3">
+                    <a class="nav-link text-white" href="#">Tasks</a>
+                </li>
+                <div class="vr text-white"></div>
+                <li class="nav-item px-3">
+                    <a class="nav-link text-white" href="#">Charts</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item px-3">
+                    <a class="nav-link text-white" href="#"><img src="" alt="Profile"></a>
+                </li>
+                <div class="vr text-white"></div>
+                <li class="nav-item px-3">
+                    <a class="nav-link text-white" href="#">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!--main-->
+<div class="container-fluid px-5 py-3">
+  <h1>SDS Projects</h1>
+  <div class="row row-cols-auto">
+    <div class="col py-2 pe-3">
+      <a class="link-underline link-underline-opacity-0" href="#">
+    <div class="card shadow mb-5" style="width: 20rem; height: 20rem;">
+        <div class="card-body">
+            <h4>bXTRA Site</h4>
+            <p class="card-text">Nisi id qui esse in ea ut enim ut dolore sint consectetur magna.</p>
+        </div>
+    </div>
+</a>
+    </div>
+    <div class="col py-2 pe-3">
+      <a class="link-underline link-underline-opacity-0" href="#">
+    <div class="card shadow mb-5" style="width: 20rem; height: 20rem;">
+        <div class="card-body">
+            <h4>bXTRA Site</h4>
+            <p class="card-text">Nisi id qui esse in ea ut enim ut dolore sint consectetur magna.</p>
+        </div>
+    </div>
+</a>
+    </div>
+    <div class="col py-2 pe-3">
+      <a class="link-underline link-underline-opacity-0" href="#">
+    <div class="card shadow mb-5" style="width: 20rem; height: 20rem;">
+        <div class="card-body">
+            <h4>bXTRA Site</h4>
+            <p class="card-text">Nisi id qui esse in ea ut enim ut dolore sint consectetur magna.</p>
+        </div>
+    </div>
+</a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
