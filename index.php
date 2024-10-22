a1e97feb18e444a8d40c28dba05ff8fef51d6040
degenerasian <tycedric.a@gmail.com>
2024-10-22 16:40:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
+
+    <?php
+        session_start();
+        require_once("modules/nav.php");
+        require_once "services/db_config.php";
+        get_navbar();
+    ?>
     <body class="bg-body-tertiary">
 
         <!--nav-->
