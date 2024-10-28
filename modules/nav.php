e2f90bef01bc6ccbb63febaf64c51318de50b697
degenerasian <tycedric.a@gmail.com>
2024-10-28 13:23:30 +0800

    Update nav.php

--- modules/nav.php
+++ modules/nav.php
@@ -9,7 +9,7 @@
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><img src="SDSTaskSite/img/logo.png" alt="SDS Logo" height='33px'></a>
+        <a class="navbar-brand" href="index.php"><img src="/img/logo.png" alt="SDS Logo" height='33px'></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -27,6 +27,10 @@
                         <div class="vr text-white"></div>
                         <li class="nav-item px-3">
                             <a class="nav-link text-white" href="../register.php">Register New User</a>
+                        </li>
+                        <div class="vr text-white"></div>
+                        <li class="nav-item px-3">
+                            <a class="nav-link text-white" href="../register.php">Create New Project</a>
                         </li>
                     <?php }
                 ?>
